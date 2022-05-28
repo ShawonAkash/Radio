319857d655d2d8ef9a161df125012e020a061d9b
ShawonAkash <mdshawonakash@gmail.com>
2022-05-28 21:26:18 +0600

    Login componenet created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import Radio from "./components/radio/Radio";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
+import Radio from './components/radio/Radio'
+import Login from './components/Login/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className='w-full h-[1200px] text-[#a2abbd] leading-[80px] bg-gradient-to-b from-[#21212b] to-[#21212b] pt-[100px]'>
-        <Radio />
-      </div>
-    );
+      <Router>
+        <div className='w-full h-[1200px] text-[#a2abbd] leading-[80px] bg-gradient-to-b from-[#21212b] to-[#21212b] pt-[20px]'>
+          <Route path='/' component={Radio} exact />
+          <Route path='/login' component={Login} />
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
